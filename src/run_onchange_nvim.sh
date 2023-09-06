32d165d629ed395aac3aed8017e9fe9abd663b89
Axel Yung <axelyung@gmail.com>
2023-09-06 09:40:17 -0700

    scripts

--- src/run_onchange_nvim.sh
+++ src/run_onchange_nvim.sh
@@ -1,5 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 
-source ~/.zshenv
+echo "Running nvim setup..."
 
+set -eux
+
+# nvim hash: {{ include ".config/nvim" | sha256sum }}
 nvim +PlugUpgrade +PlugInstall +CocUpdate +CocInstall +qall
