f8a251e32e9dcfd2fd23d8658797f92c97a36107
Axel Yung <axelyung@gmail.com>
2023-09-07 14:06:59 -0700

    fix

--- .chezmoiscripts/run_after_onchange_asdf.sh
+++ .chezmoiscripts/run_after_onchange_asdf.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# .tool-versions hash: {{ include ".tool-versions" | sha256sum }}
+
 if ! which asdf; then
 	echo "asdf not installed, skipping setup..."
 	exit 0
 fi
 
-# .tool-versions hash: {{ include ".tool-versions" | sha256sum }}
-if [ -f ~/.tool-versions ]; then
-	echo "Running asdf setup..."
-	asdf install
-	asdf reshim
-else
+if [ ! -f ~/.tool-versions ]; then
 	echo "~/.tool-versions file does not exist, skipping setup..."
+	exit 0
 fi
 
+echo "Running asdf setup..."
+
+plugins=$(cat ~/.tool-versions | awk '{print $1}')
+for plugin in $plugins; do
+	asdf plugin add $plugin
+done
+
+asdf install
+asdf reshim
