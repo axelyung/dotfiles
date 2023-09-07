774766c970974cc1863a36fdc1affdc361e9448d
Axel Yung <axelyung@gmail.com>
2023-09-07 09:50:48 -0700

    refactor

--- .chezmoiscripts/run_after_onchange_asdf.sh
+++ .chezmoiscripts/run_after_onchange_asdf.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Running asdf setup..."
+if ! which asdf; then
+	echo "asdf not installed, skipping setup..."
+	exit 0
+fi
 
 # .tool-versions hash: {{ include ".tool-versions" | sha256sum }}
-[ -f ~/.tool-versions ] && asdf install
+if [ -f ~/.tool-versions ]; then
+	echo "Running asdf setup..."
+	asdf install
+	asdf reshim
+else
+	echo "~/.tool-versions file does not exist, skipping setup..."
+fi
+
