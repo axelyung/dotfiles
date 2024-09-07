141433a431188162bcd4c46005d3504e59cc15e9
Axel Yung <axel@nominal.io>
2024-09-06 23:24:42 -0700

    updates

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -6,4 +6,12 @@ return {
     { "<leader>/", vim.NIL },
     { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (cwd)" },
   },
+  vimgrep_arguments = {
+    "rg",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+  },
 }
